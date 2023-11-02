eb802dfc00fe0bbabd090f503e71a5b647fea742
thientm27 <denkhotieu1@gmail.com>
2023-11-02 16:21:24 +0700

    change to deployment api

--- DriverLicenseExamLearning-gRPC/ApiConstans.cs
+++ DriverLicenseExamLearning-gRPC/ApiConstans.cs
@@ -3,6 +3,6 @@
     public static class ApiConstans
     {
         public static string ApiCheckStaff = "https://localhost:7018/api/user/IsStaff";
-        public static string ApiCheckStaffDeploy = "https://localhost:7018/api/user/IsStaff";
+        public static string ApiCheckStaffDeploy = "https://www.driverlicenseexamlearning.somee.com/api/user/IsStaff";
     }
 }
